34c067d2e6d09edbeddb15f279c04287ea5e526b
Chen Kai Xuan <kche0147@student.monash.edu>
2023-10-05 00:06:32 +1100

    embed the bubble chart and put in the webpage

--- FIT3179_DV2/mapWithGraticules.js
+++ FIT3179_DV2/mapWithGraticules.js
@@ -2,3 +2,8 @@ var vg_1 = "./js/UK_symbolmap_with_graticules.json";
 vegaEmbed("#symbol_map", vg_1)
   .then(function (result) {})
   .catch(console.error);
+
+var vg_2 = "./js/interactive_bubblechart.json";
+vegaEmbed("#bubble", vg_2)
+    .then(function (result) {})
+    .catch(console.error);
