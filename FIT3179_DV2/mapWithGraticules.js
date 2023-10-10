f5a628ef834b30c8aedfb44a8aed380841fd9c9c
Chen Kai Xuan <kche0147@student.monash.edu>
2023-10-10 23:51:34 +1100

    edited the satisfaction

--- FIT3179_DV2/mapWithGraticules.js
+++ FIT3179_DV2/mapWithGraticules.js
@@ -7,3 +7,18 @@ var vg_2 = "./js/interactive_bubblechart.json";
 vegaEmbed("#bubble", vg_2)
     .then(function (result) {})
     .catch(console.error);
+    
+var vg_3 = "./js/interactive_bubblechart.json";
+vegaEmbed("#bubble", vg_3)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_4 = "./js/interactive_bubblechart.json";
+vegaEmbed("#bubble", vg_4)
+    .then(function (result) {})
+    .catch(console.error);
+
+var vg_5 = "./js/interactive_bubblechart.json";
+vegaEmbed("#bubble", vg_5)
+    .then(function (result) {})
+    .catch(console.error);
