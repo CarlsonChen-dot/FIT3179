fd98055e81252a59935b915b41469c0f92cea7ba
Chen Kai Xuan <kche0147@student.monash.edu>
2023-10-15 16:22:39 +1100

    final version

--- FIT3179_DV2/mapWithGraticules.js
+++ FIT3179_DV2/mapWithGraticules.js
@@ -1,34 +1,34 @@
 var vg_1 = "./js/UK_symbolmap_with_graticules.json";
 vegaEmbed("#symbol_map", vg_1)
-  .then(function (result) {})
-  .catch(console.error);
+    .then(function (result) { })
+    .catch(console.error);
 
 var vg_2 = "./js/histogram.json";
 vegaEmbed("#histo", vg_2)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
-    
+
 var vg_3 = "./js/bot5Uni.json";
 vegaEmbed("#bot5", vg_3)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
 
 var vg_4 = "./js/top5Uni.json";
 vegaEmbed("#top5", vg_4)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
 
 var vg_5 = "./js/interactive_scatterplot.json";
 vegaEmbed("#scatter", vg_5)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
 
 var vg_6 = "./js/pyramidchart.json";
 vegaEmbed("#pyramid", vg_6)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
 
 var vg_7 = "./js/piechart.json";
 vegaEmbed("#pie", vg_7)
-    .then(function (result) {})
+    .then(function (result) { })
     .catch(console.error);
