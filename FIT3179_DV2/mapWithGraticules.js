7537f8b694562c1121da10211cea8d2514552d27
Chen Kai Xuan <kche0147@student.monash.edu>
2023-10-15 20:53:09 +1100

    final update

--- FIT3179_DV2/mapWithGraticules.js
+++ FIT3179_DV2/mapWithGraticules.js
@@ -1,34 +1,34 @@
 var vg_1 = "./js/UK_symbolmap_with_graticules.json";
-vegaEmbed("#symbol_map", vg_1)
+vegaEmbed("#symbol_map", vg_1, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_2 = "./js/histogram.json";
-vegaEmbed("#histo", vg_2)
+vegaEmbed("#histo", vg_2, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_3 = "./js/bot5Uni.json";
-vegaEmbed("#bot5", vg_3)
+vegaEmbed("#bot5", vg_3, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_4 = "./js/top5Uni.json";
-vegaEmbed("#top5", vg_4)
+vegaEmbed("#top5", vg_4, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_5 = "./js/interactive_scatterplot.json";
-vegaEmbed("#scatter", vg_5)
+vegaEmbed("#scatter", vg_5, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_6 = "./js/pyramidchart.json";
-vegaEmbed("#pyramid", vg_6)
+vegaEmbed("#pyramid", vg_6, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
 
 var vg_7 = "./js/piechart.json";
-vegaEmbed("#pie", vg_7)
+vegaEmbed("#pie", vg_7, {renderer: "svg", actions: false})
     .then(function (result) { })
     .catch(console.error);
